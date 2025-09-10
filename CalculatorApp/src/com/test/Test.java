607fcfd30fcbb401033b3d83dd1f27a6cd49bba6
cjcnkolhe <103464730+cjcnkolhe@users.noreply.github.com>
2025-09-10 12:54:51 +0530

    add method by devC

--- CalculatorApp/src/com/test/Test.java
+++ CalculatorApp/src/com/test/Test.java
@@ -1,6 +1,9 @@
 package com.test;
 
 public class Test {
+	public void add(){
+        System.out.println(10+20);
+	}
 	public static void main(String[] args) {
 		System.out.println("CalculatorApp");
 	}
